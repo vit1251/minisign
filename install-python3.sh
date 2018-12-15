005c17d3d32ff256ba1f43257d3ec17ade06833f
Vitold S <vit1251@gmail.com>
2018-12-15 11:08:36 +0300

    Update Python version up 3.6.7

--- install-python3.sh
+++ install-python3.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
-tar xf Python-3.6.1.tar.xz
-cd Python-3.6.1
+wget https://www.python.org/ftp/python/3.6.7/Python-3.6.7.tar.xz
+tar xf Python-3.6.7.tar.xz
+cd Python-3.6.7
 ./configure --enable-optimizations
-make -j 8
+make -j 2
 sudo make altinstall
